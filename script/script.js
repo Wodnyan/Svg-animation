ef46aebe70cfb7123255aa94b2a006b4f09636cb
Wodnyan <52217740+Wodnyan@users.noreply.github.com>
2019-08-27 01:52:53 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let logo = document.querySelectorAll('#logo path');
-
+//Determines the length of each path
 for (let i = 0; i < logo.length; i++) {
     console.log(`${i} is ${logo[i].getTotalLength()} long`);
 
